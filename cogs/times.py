5d2459c27f54789622b58836cf635358b681ec84
EBlTEN <63699174+EBlTEN@users.noreply.github.com>
2023-06-01 20:22:34 +0900

    動作の改善
    エラーメッセージの動作を改善
    可読性の改善

--- cogs/times.py
+++ cogs/times.py
@@ -3,7 +3,12 @@ import os
 from logging import getLogger
 
 import dotenv
-from discord import Embed, HTTPException, Message, MessageType
+from discord import (
+    Embed,
+    HTTPException,
+    Message,
+    MessageType,
+)
 from discord.ext import commands
 
 logger = getLogger(f"discord.{__name__}")
@@ -25,94 +30,82 @@ class Times(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def extract_args():
-        pass
-
-    @commands.Cog.listener()
-    async def on_ready(self):
+    async def cog_load(self):
         logger.info("File has been loaded successfully")
 
     # リアクションがつけられたときに呼ばれる
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        # 変数
         emoji = str(payload.emoji)
         channel = self.bot.get_channel(payload.channel_id)
         message = await channel.fetch_message(payload.message_id)
         guild = self.bot.get_guild(message.guild.id)
         user = guild.get_member(payload.user_id)
-        try:
-            # 分報カテゴリであるか
-            # 自分のメッセージにつけたリアクションであるか
-            if (
-                channel.category.id in config["times_category"]
-                and message.author.id == user.id
-                and emoji == "📌"
-            ):
-                # リアクションが赤ピンならピン留め
+
+        if not channel.category:
+            return
+
+        if not channel.category.id in config["times_category"]:
+            return
+
+        if not emoji == "📌":
+            return
+
+        if message.author.id == user.id:
+            try:
                 await message.pin()
                 logger.info("%s pinned message", user)
 
-            # カテゴリ内の他人につけたリアクションであるか
-            elif (
-                channel.category.id in config["times_category"]
-                and message.author.id != user.id
-                and emoji == "📌"
-            ):
-                # 赤ピンがついたら削除
+            # ピン留めに失敗した時
+            except HTTPException as err:
+                embed = Embed(
+                    title="Error", description="メッセージのピン留めに失敗しました｡", color=0xCC3333
+                )
+                embed.add_field(name=err.status, value=err.text)
+                embed.set_footer(text=f"Error code:{err.code}")
+                await channel.send(embed=embed)
                 await message.remove_reaction(emoji, user)
+                logger.error(err)
 
-            # カテゴリ外のリアクションには反応しない
-            else:
-                pass
-        # カテゴリに属さないチャンネルのカテゴリidを参照したとき
-        except AttributeError:
-            pass
-        # ピン留めに失敗したとき
-        except HTTPException as err:
-            # 埋め込みメッセージの作成
-            embed = Embed(
-                title="Error", description="メッセージのピン留めに失敗しました｡", color=0xCC3333
-            )
-            embed.add_field(name=err.status, value=err.text)
-            embed.set_footer(text=f"Error code:{err.code}")
-            await channel.send(embed=embed)
+        else:
             await message.remove_reaction(emoji, user)
-            logger.error(err)
 
     # リアクションが削除されたときに呼び出される｡
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
-        # 変数
         emoji = str(payload.emoji)
         channel = self.bot.get_channel(payload.channel_id)
         message = await channel.fetch_message(payload.message_id)
         guild = self.bot.get_guild(message.guild.id)
         user = guild.get_member(payload.user_id)
 
-        try:
-            # 分報カテゴリかつ赤ピンが削除された
-            if (
-                channel.category.id in config["times_category"]
-                and message.author.id == user.id
-                and emoji == "📌"
-            ):
+        if not channel.category:
+            return
+
+        if not message.pinned:
+            return
+
+        if not channel.category.id in config["times_category"]:
+            return
+
+        if not emoji == "📌":
+            return
+
+        if message.author.id == user.id:
+            print(message.author.id, user.id)
+            try:
                 await message.unpin()
                 logger.info("%s unpinned message", user)
-            else:
-                pass
-        except AttributeError:
-            pass
-        # ピン留めに失敗したとき
-        except HTTPException as err:
-            # 埋め込みメッセージの作成
-            embed = Embed(
-                title="Error", description="メッセージのピン留めの解除に失敗しました｡", color=0xCC3333
-            )
-            embed.add_field(name=err.status, value=err.text)
-            embed.set_footer(text=f"Error code:{err.code}")
-            await channel.send(embed=embed)
-            logger.error(err)
+
+            # ピン留めに失敗したとき
+            except HTTPException as err:
+                embed = Embed(
+                    title="Error", description="メッセージのピン留めの解除に失敗しました｡", color=0xCC3333
+                )
+                embed.add_field(name=err.status, value=err.text)
+                embed.set_footer(text=f"Error code:{err.code}")
+                await channel.send(embed=embed)
+                logger.error(err)
 
     @commands.Cog.listener()
     async def on_message(self, message: Message):
@@ -121,7 +114,6 @@ class Times(commands.Cog):
             message.type == MessageType.pins_add
             and message.channel.category_id in config["times_category"]
         ):
-            # 削除
             await message.delete()
 
 
